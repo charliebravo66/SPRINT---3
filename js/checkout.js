967bd0829b1b666e6ac9466d0d4e6eaa22028675
Guidarom <guidodaniel.davila@gmail.com>
2022-11-09 09:10:32 +0100

    update

--- js/checkout.js
+++ js/checkout.js
@@ -1,67 +1,149 @@
 
 // Exercise 6
-function validate() {
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+
+function validar() {
+
 	var error = 0;
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fLastN = document.getElementById("fLastN");
-	var fEmail = document.getElementById("fEmail");
-	
-	var fLastN = document.getElementById("fLastN");
+	// crear variables de los inputs 
+	let fName = document.getElementById("fName");
+	let fLastN = document.getElementById("fLastN");
+	let fEmail = document.getElementById("fEmail");
+	let fPassword = document.getElementById("fPassword");
+	let fAddress = document.getElementById("fAddress");
+	let fPhone = document.getElementById("fPhone");
 
-	let validaTodo = document.forms["formulario_registro"].value;
-	if (validaTodo == "") {
-	  alert("Debes rellenar todos los campos del formulario");	  
-	  return false;
-	}
 
-	//let x = document.forms["myForm"]["fname"].value;
-	//if (fName.value == "") {
-	let valor = document.getElementById("fName").value;
-		if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {	
-	  alert("Debes rellenar el campo Nombre");
-	  fName.focus();
-	  //document.getElementById("fName").style.backgroundColor = "red";
-	  return false;
-	}
-	
 	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorLastN = document.getElementById("errorLastN");
-	var errorEmail = document.getElementById("errorEmail"); 
-	var errorPassword = document.getElementById("errorPassword");
-	var errorAddress = document.getElementById("errorAddress");
-	var errorPhone = document.getElementById("errorPhone");      
+	let errorName = document.getElementById("errorName");
+	let errorLastN = document.getElementById("errorLastN"); 
+	let errorEmail = document.getElementById("errorEmail");
+	let errorPassword = document.getElementById("errorPassword"); 
+	let errorAddress = document.getElementById("errorAddress"); 
+	let errorPhone = document.getElementById("errorPhone");   
 	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		let valor = document.getElementById("fName").value;
-		if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) 
-		{ 
+	let valNombre = document.getElementById("fName").value;
+	let valApellido = document.getElementById("fLastN").value;
+
+	let validaMail = RegExp(/^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i);
+	let validaPass = RegExp(/^(?=.*[0-9])(?=.*[a-z]).{3,8}/);
+	let validaPhone = RegExp(/^[0-9]{3,}$/);
+
+	
+	// validación input nombre
+	if(valNombre == null || valNombre.length == 0 || /^\s+$/.test(valNombre)) {	
+	  	alert("primera validación");
+			fName.focus();
+			alert("mal");
+			errorName;
+			
+			fName.classList.remove("is-invalid");
+			fName.classList.add("is-valid");
+
+			return false;
+			
+	} else {
+		//alert("bien nombre");
+        fName.classList.add("is-invalid");
+        fName.classList.remove("is-valid");
+
+		return valNombre;
+	}
+
+}
+	// validación input apellido
+	if(valApellido == null || valApellido.length == 0 || /^\s+$/.test(valApellido)) {	
+		alert("primera validación");
+		fLastN.focus();
+		errorLastN;
+		  alert("mal");
+		  fLastN.classList.add("is-valid");
+		  fLastN.classList.remove("is-invalid");
+		  return false;
+		  
+  } else {
+	  alert("bien");
+	  fLastN.classList.add("is-invalid");
+	  fLastN.classList.remove("is-valid");
+	  return fLastN;
+	  
+  }
+
+
+	// validación input mail
+	let validaMail = RegExp(/^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i);
+	
+	if (!validaMail.test(fEmail)){
+		//error = 1;
+		document.formulario_registro.fEmail.focus();
+
+		fEmail.classList.add("is-valid");
+		fEmail.classList.remove("is-invalid");
 		return false;
-		}
-		error++;
+
+	}  else {
+
+		fEmail.classList.add("is-invalid");
+		fEmail.classList.remove("is-valid");
+		return fEmail;
 	}
 
 	if(fEmail.value == ""){
-		alert("Falta rellenar Email");
+
 		fEmail.focus();
-		document.getElementById("fName").style.backgroundColor = "red";
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
+		fEmail.classList.add("is-valid");
+		fEmail.classList.remove("is-invalid");
+		return false;
 
-	if(fPhone.value == ""){
-		let valorTelefono = document.getElementById("campo").value;if( !(/^\d{10}$/.test(valor)) ) 
-		{ return false;}	
-		error++;
+	} else{	
+
+		fPhone.classList.add("is-invalid");
+		fPhone.classList.remove("is-valid");
+		return fEmail;
 	}
 
+	// validación input password
+	if(!validaPass.test(fPassword)){
+
+		fPassword.classList.add("is-valid");
+		fPassword.classList.remove("is-invalid");
+		return false;
+
+	} else{
+
+		fPassword.classList.add("is-invalid");
+		fPassword.classList.remove("is-valid");
+		return fPassword;
+	}
+
+	// validación input phone
+	if(isNaN(fPhone) || !validaPhone.test(fPhone)) {
+
+		fPhone.classList.add("is-valid");
+		fPhone.classList.remove("is-invalid");
+		return false;
+	} else{
+		alert("bien");
+		fPhone.classList.add("is-invalid");
+		fPhone.classList.remove("is-valid");
+		return fPhone;	
+	}
+
+enviado();
 
 
+
+
+
+
+
+
+/*limpiarForm(){
+	document.getElementById('formulario_registro').reset();
+}*/
+function enviado(){
+	alert("Formulario Completo enviado !!")
+	document.location.href="checkout.html";
 }
+	
